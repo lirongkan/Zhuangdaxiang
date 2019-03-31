1216e8e790fb605b64e87bb59fbe5c1fbd5f731e
lirongkan <33339324+lirongkan@users.noreply.github.com>
2019-03-31 15:28:55 +0800

    test

---
+++ src/arrays/test.java
@@ -0,0 +1,13 @@
+package src.arrays;
+
+/**
+ * This class show you how to write right tags
+ *
+ * @author: lrk
+ * 2019/3/31 10:38
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
