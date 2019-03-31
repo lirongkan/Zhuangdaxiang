1898e8b8a521fe6e82603058a7d515178483f39b
lirongkan <33339324+lirongkan@users.noreply.github.com>
2019-03-31 16:01:57 +0800

    test9

--- src/io/test/Test1.java
+++ src/io/test/Test1.java
@@ -7,5 +7,10 @@ package io.test;
 public class Test1 {
     public static void main(String[] args) {
     System.out.println("a");
+
+    System.out.println(121212);
+    System.out.println("纯属娱乐" +
+            "哈哈哈");
+
     }
 }
